a619caf6a5229f91bba1a917dec0faeb0221cf53
Niko Finlertet <nikofinlertet@gmail.com>
2024-11-10 21:20:55 +0300

    work in process

---
+++ nameless/src/test/java/arcan/nameless/NamelessApplicationTests.java
@@ -0,0 +1,13 @@
+package arcan.nameless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamelessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
