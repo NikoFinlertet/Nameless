3ebd63f17b8ed802a1a692f71070086d313c15ff
Niko Finlertet <nikofinlertet@gmail.com>
2024-11-29 16:08:47 +0300

    Frontend up

---
+++ frontend/src/ContentCard/ContentCard.js
@@ -0,0 +1,16 @@
+import './ContentCard_Style.css';
+
+
+const ContentCard = ({content}) => {
+    const {type, title, description, preview_img, mediaURL} = content;
+
+    return(
+        <div>
+
+        </div>
+    );
+};
+
+export default ContentCard;
+
+
