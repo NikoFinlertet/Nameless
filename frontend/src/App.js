a86a7df4cacac4650dc134c90b170aeb04f23192
Niko Finlertet <nikofinlertet@gmail.com>
2024-12-05 13:01:40 +0300

    something

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ContentList />
+        <div className="ContentArea">
+          <ContentList></ContentList>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
